270572ac5c559c147e5283e3053d74ec45222514
ravi-manchala <ravikumar.manchala95@gmail.com>
2021-10-26 23:06:19 +0530

    filter2

--- src/Components/Filters/Filters.js
+++ src/Components/Filters/Filters.js
@@ -16,12 +16,14 @@ const arr = [
 const Filters = () => {
   const [options, setOptions] = useState([]);
   const [bucketData, setBucketData] = useState();
+  const [suggestions, setSuggestions] = useState();
 
   const fetchData = () => {
     Axios.get(
       "https://listing-service.qac24svc.dev/v1/filter?&variant=filterV3",
       { headers: { X_COUNTRY: "AE", X_VEHICLE_TYPE: "CAR" } }
     ).then((response) => {
+      // console.log(response);
       const result = arr.map((ele) => {
         return response.data.filters[ele];
       });
@@ -33,33 +35,61 @@ const Filters = () => {
     fetchData();
   }, []);
 
-  // console.log(response);
   console.log(options);
 
   const getBuckets = (id) => {
-    // console.log(options[id].buckets);
     setBucketData(options[id].buckets);
+    setSuggestions(options[id].suggestions);
   };
 
+  console.log(suggestions);
+
   return (
-    <div>
-      {options.map((option, index) => {
-        return (
-          <div key={option.name}>
-            <button onClick={() => getBuckets(index)}>
-              {option.displayName}
-            </button>
-            {bucketData &&
-              bucketData.map((ele) => {
-                return (
-                  <div>
-                    <h6>{ele.name}</h6>
-                  </div>
-                );
-              })}
-          </div>
-        );
-      })}
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div>
+        {options.map((option, index) => {
+          return (
+            <div key={option.name}>
+              <div>
+                <button onClick={() => getBuckets(index)}>
+                  {option.displayName}
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <div>
+          {suggestions &&
+            suggestions.map((suggestion) => {
+              return (
+                <div>
+                  {suggestion.subFacet &&
+                    suggestion.subFacet.buckets.map((model) => {
+                      return (
+                        <div>
+                          <h6>
+                            {suggestion.name} {model.name}
+                          </h6>
+                        </div>
+                      );
+                    })}
+                </div>
+              );
+            })}
+        </div>
+        <div>
+          {bucketData &&
+            bucketData.map((ele) => {
+              return (
+                <div>
+                  <h6>{ele.name}</h6>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 };
